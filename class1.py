279c771c99bcb91c0c3f1aba1524c85586b61cfb
Aryan Sharma <81407258+Aryansh1520@users.noreply.github.com>
2022-05-19 18:10:44 +0530

    Update class1.py

--- class1.py
+++ class1.py
@@ -1,68 +1,111 @@
-import pygame
 import time as t 
 class giveposition():
-    def position(positionof,dienumber,player,screen):
-        l1 = [91,92,93,94,95,96,97,98,99,100]
+    def position(positionof,dienumber,player):
+        l1 = [100,99,98,97,96,95,94,93,92,91]
         l2 = [81,82,83,84,85,86,87,88,89,90]
-        l3 = [71,72,73,74,75,76,77,78,79,80]
+        l3 = [80,79,78,77,76,75,74,73,72,71]
         l4 = [61,62,63,64,65,66,67,68,69,70]
-        l5 = [51,52,53,54,55,56,57,58,59,60]
+        l5 = [60,59,58,57,56,55,54,53,52,51]
         l6 = [41,42,43,44,45,46,47,48,49,50]
-        l7 = [31,32,33,34,35,36,37,38,39,40]
+        l7 = [40,39,38,37,36,35,34,33,32,31]
         l8 = [21,22,23,24,25,26,27,28,29,30]
-        l9 = [11,12,13,14,15,16,17,18,19,20]
+        l9 = [20,19,18,17,16,15,14,13,12,11]
         l10 = [1,2,3,4,5,6,7,8,9,10]
-
+        print(player," die number : " ,dienumber)
         positionof = positionof+dienumber
 #Ladders
-        if  positionof == 5:
-            lx = 10 + (l7.index(32)*65)
+        if  positionof == 10:#done
+            lx = 10 + (l7.index(33)*65)
             ly = 405
-            positionof = 32
-        elif positionof == 75:
-            lx = 10 + (l1.index(97)*65)
-            ly = 15
-            positionof = 97
-        elif positionof == 59:
-            lx = 10 + (l1.index(99)*65)
-            ly = 15
-            positionof = 99
-#Snakes
-        elif positionof ==91:
+            print(player,"climbed from 10 to 33")
+            positionof = 33
+        elif positionof == 16:#done
+            lx = 10 + (l7.index(37)*65)
+            ly = 405
+            positionof = 37
+            print(player,"climbed from 16 to 37")
+
+        elif positionof == 21:#done
+            lx = 10 + (l6.index(41)*65)
+            ly = 340
+            positionof = 41
+            print(player,"climbed from 21 to 41")
+
+        elif positionof ==35:#done
             lx = 10 + (l5.index(54)*65)
             ly = 275
             positionof = 54
+            print(player,"climbed from 35 to 54")
 
-        elif positionof == 78:
-            lx = 10+(l5.index(55)*65)
-            ly = 275
-            positionof = 55
+        elif positionof == 44:#done
+            lx = 10+(l3.index(76)*65)
+            ly = 115
+            positionof = 76
+            print(player,"climbed from 44 to 76")
 
-        elif positionof == 36:
-            lx = 10+(l8.index(25)*65)
-            ly = 470
-            positionof = 25
+        elif positionof == 80:#done
+            lx = 10+(l1.index(99)*65)
+            ly = 15
+            positionof = 99
+            print(player,"climbed from 80 to 99")
 
-        elif positionof == 40:
-            lx = 10+(l9.index(19)*65)
+
+#snakes
+        elif positionof == 23:#done
+            lx = 10+(l10.index(2)*65)
+            ly = 600
+            positionof = 2
+            print(player,"was cut at 23 and placed at 2")
+
+        elif positionof == 34:#done
+            lx = 10+(l9.index(15)*65)
             ly = 535
-            positionof = 19
+            positionof = 15
+            print(player,"was cut at 34 and placed at 15")
+
+        elif positionof == 52:
+            lx = 10+(l7.index(31)*65)
+            ly = 405
+            positionof = 31
+            print(player,"was cut at 52 and was placed at 31")
+
+        elif positionof == 62:
+            lx = 10+(l6.index(43)*65)
+            ly = 405
+            positionof = 43
+            print(player,"was cut 62 and placed at 43")
+
+        elif positionof == 89:
+            lx = 10+(l4.index(68)*65)
+            ly = 210
+            positionof = 68
+            print(player,"was cut at 89 and placed at 68")
+
+        elif positionof == 95:
+            lx = 10+(l3.index(74)*65)
+            ly = 115
+            positionof = 74
+            print(player,"was cut 95 and placed at 74")
+
+
+
 
         elif positionof >100:
             positionof = 100 - (positionof -100)
             lx = 10 + (l1.index(positionof)*65)
             ly = 15
+
         elif positionof == 100:
             lx = 10+(l1.index(positionof)*65)
             ly = 15
             if player == "blue":
-                print("Blue Won!Closing in 5 seconds")
+                print("**********Blue Won!Closing in 5 seconds**********")
                 t.sleep(5)
                 quit()
             else:
                 lx = 10+(l1.index(positionof)*65)
                 ly = 15
-                print("Yellow Won! Closing in 5 seconds")
+                print("**********Yellow Won! Closing in 5 seconds**********")
                 t.sleep(5)
                 quit()
 
